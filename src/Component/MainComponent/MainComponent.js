fabb2ea00c66c129d6691e646e13c891ae341d18
Bittu singh <bittusingh8991@gmail.com>
2023-12-23 02:27:18 +0530

    TermsDone

--- src/Component/MainComponent/MainComponent.js
+++ src/Component/MainComponent/MainComponent.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "../Header/Header";
 import { NavHome } from "../Features/NavHome";
-import { Box } from "@mui/material";
 import Footer from "../Footer/Footer";
-import Terms from "../Terms&Condition/Terms";
 
 const MainComponent = ()=>{
   return(
     <Fragment>
       <Header/>
-      {/* <NavHome/> */}
-      <Terms />
+      <NavHome/>
       <Footer />
     </Fragment>
   )
