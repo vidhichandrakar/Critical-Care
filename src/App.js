e109555760a7d1f694296b8d6ef638403070bdd8
Shrishti2698 <shrishti260498@gmail.com>
2023-12-23 19:16:50 +0530

    sh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import MainComponent from './Component/MainComponent/MainComponent';
+import ContactUs from './Component/Service/ContactUs';
+import PrivacyPolicy from './Component/Service/PrivacyPolicy';
+
+
 function App() {
   return (
     <div className="App">
-      <MainComponent/>
+      {/* <MainComponent/> */}
+      <ContactUs/>
+      <PrivacyPolicy/>
     </div>
   );
 }
