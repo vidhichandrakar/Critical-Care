68a144b39aeec4a9c084c422089be0d70308ce97
vidhi chandrakar <vidhichandrakar.1701@gmail.com>
2024-03-02 01:49:46 +0530

    overflow

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -47,7 +47,7 @@ function Header() {
     <AppBar position="static" className="AppHeader">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <Box sx={{  display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
